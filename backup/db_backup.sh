757169ac2f85c8f8d478325e8c4e9fed5fd1f051
ahkui <ahkui@outlook.com>
2019-10-01 13:55:04 +0800

    Update Backup Scripts

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -1,2 +1,2 @@
 cd /home/iecsadm/OnlineJudgeDeploy
-/usr/local/bin/docker-compose exec -T oj-postgres pg_dumpall -c -U onlinejudge > /home/iecsadm/oj_backup/db_backup_`date +%Y_%m_%d"_"%H_%M_%S`.sql 2>&1
+/usr/local/bin/docker-compose exec -T oj-postgres pg_dumpall -c -U onlinejudge > /home/iecsadm/gdrive/oj_backup/db_backup_`date +%Y_%m_%d"_"%H_%M_%S`.sql 2>&1
